546a79accfd1a3cc35fcf36cec7364fd63559e73
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2021-12-02 06:31:15 -0300

    Renomeação de arquivo.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,24 +2,31 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin', '~/.local/share/luanvim/plugged')
 
-Plug 'morhetz/gruvbox'
+-- Misc
 Plug 'sheerun/vim-polyglot'
 Plug 'tpope/vim-surround'
 Plug 'tpope/vim-commentary'
-
+Plug 'sunaku/vim-shortcut'
 Plug ('junegunn/fzf', {
-    -- ['do'] = [[./install --all --no-zsh]],
     ['do'] = function ()
       vim.call('fzf#install')
     end,
     ['dir'] = [[~/.fzf]]
   })
 Plug 'junegunn/fzf.vim'
+Plug 'mhinz/vim-startify'
+Plug 'ryanoasis/vim-devicons'
+Plug ('scrooloose/nerdtree', {on = {'NERDTreeToggle', 'NERDTreeFind'}})
+Plug ('tiagofumo/vim-nerdtree-syntax-highlight', {on = {'NERDTreeToggle', 'NERDTreeFind'}})
+Plug 'vimwiki/vimwiki'
+Plug 'RRethy/vim-illuminate'
 
 -- Git
 Plug 'tpope/vim-fugitive'
 Plug 'airblade/vim-gitgutter'
 Plug 'rhysd/git-messenger.vim'
-Plug 'sunaku/vim-shortcut'
+
+-- Colorscheme
+Plug 'morhetz/gruvbox'
 
 vim.call('plug#end')
