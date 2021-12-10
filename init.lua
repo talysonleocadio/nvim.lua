6a599d4ac56309adf78f7bd2a2c7daa684c49104
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2021-12-09 22:10:04 -0300

    Remoção de carregamento das confs de plugins na incialização do editor.
    Todas as confs são gerenciadas diretamente pelo packer.

--- init.lua
+++ init.lua
@@ -1,12 +1,3 @@
 require('plugins')
 require('settings')
 require('keybinds')
-local utils = require('utils')
-
-local settings_path = vim.fn['stdpath']('config') .. '/lua/plugin-settings'
-local list_settings_files_command = 'ls ' .. settings_path
-
-for file in io.popen(list_settings_files_command):lines() do
-  local setting = utils.split_by_dot_and_get_setting_name(file)
-  require('plugin-settings.' .. setting)
-end
