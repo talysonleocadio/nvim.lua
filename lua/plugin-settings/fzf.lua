603beacf71e21769d803ac04f78455a4472667c5
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2021-12-09 22:20:30 -0300

    Deleção de confs de plugins não mais usadas.
    Obrigado por tudo fzf, NERDTree vim-shortcut.

--- lua/plugin-settings/fzf.lua
+++
@@ -1,9 +0,0 @@
--- Set env variable for file searching
-vim.env.FZF_DEFAULT_COMMAND = [[rg --files --hidden --no-messages --glob="!{**/*.min.js,**/*.min.css,.git/*}"]]
-
--- Plugin options
-vim.g.fzf_action = {
-  ['ctrl-t'] = 'tab split',
-  ['ctrl-s'] = 'split',
-  ['ctrl-v'] = 'vsplit'
-}
