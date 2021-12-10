08c806e2bbd458ff0a1550b5208b2be2e801b1f9
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2021-12-09 22:18:13 -0300

    Sai vim-plug (Obrigado por tudo), entra packer.
    Novo gerenciador de plugins em lua.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,32 +1,89 @@
-local Plug = vim.fn['plug#']
+vim.cmd([[
+  augroup PackerUserConfig
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
 
-vim.call('plug#begin', '~/.local/share/luanvim/plugged')
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'tpope/vim-surround'
+  use 'tpope/vim-commentary'
+  use 'andymass/vim-matchup'
+  use {
+    'mhinz/vim-startify',
+    config = function () require('plugin-settings.vim-startify-confs') end
+  }
 
--- Misc
-Plug 'sheerun/vim-polyglot'
-Plug 'tpope/vim-surround'
-Plug 'tpope/vim-commentary'
-Plug 'sunaku/vim-shortcut'
-Plug ('junegunn/fzf', {
-    ['do'] = function ()
-      vim.call('fzf#install')
-    end,
-    ['dir'] = [[~/.fzf]]
-  })
-Plug 'junegunn/fzf.vim'
-Plug 'mhinz/vim-startify'
-Plug 'ryanoasis/vim-devicons'
-Plug ('scrooloose/nerdtree', {on = {'NERDTreeToggle', 'NERDTreeFind'}})
-Plug ('tiagofumo/vim-nerdtree-syntax-highlight', {on = {'NERDTreeToggle', 'NERDTreeFind'}})
-Plug 'vimwiki/vimwiki'
-Plug 'RRethy/vim-illuminate'
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      {'kyazdani42/nvim-web-devicons'},
+    },
+    config = function () require('plugin-settings.nvim-tree-confs') end
+  }
 
--- Git
-Plug 'tpope/vim-fugitive'
-Plug 'airblade/vim-gitgutter'
-Plug 'rhysd/git-messenger.vim'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = {'kyazdani42/nvim-web-devicons'},
+    config = function () require('plugin-settings.lualine-confs') end
+  }
 
--- Colorscheme
-Plug 'morhetz/gruvbox'
+  use {
+    'vimwiki/vimwiki',
+    config = function () require('plugin-settings.vimwiki-confs') end
+  }
+  use 'RRethy/vim-illuminate'
 
-vim.call('plug#end')
+  use 'tpope/vim-fugitive'
+  use 'airblade/vim-gitgutter'
+  use 'rhysd/git-messenger.vim'
+
+  use 'morhetz/gruvbox'
+
+  use {
+    'neovim/nvim-lspconfig',
+    requires = {
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'onsails/lspkind-nvim'
+    },
+    config = function () require('plugin-settings.lsp-confs') end
+  }
+
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    config = function () require('plugin-settings.nvim-treesitter-confs') end
+  }
+
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {
+      {'nvim-lua/plenary.nvim'},
+      {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    },
+    config = function () require('plugin-settings.telescope-confs') end
+  }
+
+  use {
+    'windwp/nvim-autopairs',
+    config = function () require('plugin-settings.nvim-autopairs-confs') end
+  }
+
+  use {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function () require('plugin-settings.indent-blankline-confs') end
+  }
+
+
+  use {
+    'jose-elias-alvarez/null-ls.nvim',
+    requires = {'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig'},
+    config = function () require('plugin-settings.null-ls-confs') end
+  }
+
+end)
